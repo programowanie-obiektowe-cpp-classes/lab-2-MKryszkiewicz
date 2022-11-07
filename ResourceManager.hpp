160171d737f7b1a28b03f6c80c7c45eafaef2508
MatV5 <mattikryszek@gmail.com>
2022-11-07 11:44:42 +0100

    proba naprawy

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -13,6 +13,10 @@ class ResourceManager
     {
         delete A;
     }
+    ResourceManager (const ResourceManager &B)
+    {
+        A = new Resource(*B.A);
+    }
     double get()
     {
         return (*A).get();
